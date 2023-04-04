9da5eb33fc807277e6ba147a26933edb7425ca34
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-04-04 08:19:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "401eec078d7e7ac9819450cb08497dd7"
 
+account_sid = "AC4f76975f683b0c86d385c1f0749e3779"
+auth_token = "c9d7f5d75bcd34ab51ca9f2fb78e3752"
+# auth_token = os.environ["c9d7f5d75bcd34ab51ca9f2fb78e3752"]
+
 weather_params = {
-    "lat":38.627003,
-    "lon":-90.199402,
+    "lat":38.6272700,
+    "lon":-90.1978900,
     "appid":api_key,
 }
 
@@ -25,12 +32,24 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain == True:
-    print("Looks like rain today boyzzzz, bring an umbrella with you!")
-else:
-    print("Weather looks dry for the day, no need for an umbrella BOYZZZZ!!")
-    
-
-
-
-
-
+    #print("Looks like rain today boyzzzz, bring an umbrella with you!")
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+        body="Looks like rain today boyzzzz, bring an umbrella with you!☂️",
+        from_="+18442923991",
+        to="+16185415179"
+    )
+    print(message.status)
+# else:
+#     #print("Weather looks dry for the day, no need for an umbrella BOYZZZZ!!")
+#     proxy_client = TwilioHttpClient()
+#     proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+#     client = Client(account_sid, auth_token, http_client=proxy_client)
+#     message = client.messages.create(
+#         body="Weather looks dry for the day, no need for an umbrella BOYZZZZ!!",
+#         from_="+18442923991",
+#         to="+16185415179"
+#     )
+#     print(message.status)
